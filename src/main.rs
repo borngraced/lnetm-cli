b825b67f36543880b2fab3781b08ad525796c7cc
Samuel Onoja <samiodev@icloud.com>
2023-06-06 03:30:13 +0100

    main

--- src/main.rs
+++ src/main.rs
@@ -28,12 +28,10 @@ fn main() {
         .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
         .build("/lnetm.log")
         .expect("logging init failed");
-
     let config = Config::builder()
         .appender(Appender::builder().build("logfile", Box::new(logfile)))
         .build(Root::builder().appender("logfile").build(LevelFilter::Info))
         .expect("logging init failed");
-
     log4rs::init_config(config).expect("logging init failed");
 
     let netm = NetMCli::new();
